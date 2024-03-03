838b1d60f59477f005a5dee044682c2f88548932
mohammed sebti <mohammed.sebti@agqlabs.com>
2024-03-03 14:26:39 +0100

    construsr bash.sh

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'livereload.middleware.LiveReloadScript',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'Universidad.urls'
